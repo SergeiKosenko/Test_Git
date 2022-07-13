04be87f004b47391a81829c37445853d14098011
Sergey-Kosenko <serg.cos@mail.ru>
2022-07-13 13:40:26 +0500

    1111

--- src/main/java/com/example/test_git/TestGitApplication.java
+++ src/main/java/com/example/test_git/TestGitApplication.java
@@ -3,7 +3,7 @@ package com.example.test_git;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class TestGitApplication {
 
     public static void main(String[] args) {
